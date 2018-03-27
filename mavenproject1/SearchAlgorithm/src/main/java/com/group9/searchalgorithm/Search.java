a88b1510a3869c0455bc80561d546a694b8c77fe
SanderumChristian <chund.Christianjohansen@gmail.com>
2018-03-27 15:07:53 +0200

    AStar still not done

--- mavenproject1/SearchAlgorithm/src/main/java/com/group9/searchalgorithm/Search.java
+++ mavenproject1/SearchAlgorithm/src/main/java/com/group9/searchalgorithm/Search.java
@@ -5,11 +5,20 @@
  */
 package com.group9.searchalgorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Christian
  */
 public class Search
 {
+	Map<String, Map<String, Double>> heuristic = new HashMap<String, Map<String, Double>>();
 	
+	Map<String, Double> mapA = new HashMap<>();
+	
+	public void createMaps(){
+		mapA.put("A", 0.0);
+	}
 }
