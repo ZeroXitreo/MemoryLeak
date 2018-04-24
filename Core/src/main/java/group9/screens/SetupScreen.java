7b593e3d6a4a205f30e44b48639af5f83be2d8d5
mforberg <mifor16@student.sdu.dk>
2018-04-24 15:49:33 +0200

    + and - buttons to be used for the enemies.. maybe sometime. idk

--- Core/src/main/java/group9/screens/SetupScreen.java
+++ Core/src/main/java/group9/screens/SetupScreen.java
@@ -39,32 +39,23 @@ public class SetupScreen implements Screen {
     private int size;
     private ArrayList<MovableEntity> enemies;
     private Button startButton;
+    private Button addEnemyButton;
+    private Button removeEnemyButton;
     //private ArrayList<iWeapon> weapons;
 
     public SetupScreen() {
         parentScreen = ParentScreen.getInstance();
         parentScreen.setResult();
         stage = new Stage(new ScreenViewport());
-        startButton = new TextButton("Start", parentScreen.getButtonSkin(), "default");
-        startButton.setSize(260, 36);
-        startButton.setPosition(Gdx.graphics.getWidth() / 2 - 130, Gdx.graphics.getHeight() / 2 - 150);
-        startButton.addListener(new InputListener() {
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                ParentScreen.getGame().setScreen(new GUIPlayScreen());
-            }
 
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-        });
-
-        stage.addActor(startButton);
     }
 
     @Override
     public void show() {
+
+        createStartButton();
+        createAddEnemyButton();
+        createRemoveEnemyButton();
 
         Gdx.input.setInputProcessor(stage);
     }
@@ -97,6 +88,65 @@ public class SetupScreen implements Screen {
     @Override
     public void dispose() {
         stage.dispose();
+    }
+
+    public void createStartButton() {
+        startButton = new TextButton("Start", parentScreen.getButtonSkin(), "default");
+        startButton.setSize(260, 36);
+        startButton.setPosition(Gdx.graphics.getWidth() / 2 - 130, Gdx.graphics.getHeight() / 2 - 150);
+        startButton.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                ParentScreen.getGame().setScreen(new GUIPlayScreen());
+            }
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+
+        stage.addActor(startButton);
+    }
+
+    public void createAddEnemyButton() {
+        addEnemyButton = new TextButton(" + ", parentScreen.getButtonSkin(), "default");
+        addEnemyButton.setSize(40, 30);
+        addEnemyButton.setPosition(Gdx.graphics.getWidth() / 2 + 150, Gdx.graphics.getHeight() / 2 + 50);
+        addEnemyButton.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("This should probably do something :^)");
+                //Maybe like add an enemy
+            }
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+
+        stage.addActor(addEnemyButton);
+    }
+
+    public void createRemoveEnemyButton() {
+        removeEnemyButton = new TextButton(" - ", parentScreen.getButtonSkin(), "default");
+        removeEnemyButton.setSize(40, 30);
+        removeEnemyButton.setPosition(Gdx.graphics.getWidth() / 2 + 200, Gdx.graphics.getHeight() / 2 + 50);
+        removeEnemyButton.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("This should probably do something :^)");
+                //Maybe like remove an enemy
+            }
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+
+        stage.addActor(removeEnemyButton);
     }
 
 }
